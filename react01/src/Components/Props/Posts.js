b12ab1b506a66c012866c7bc870b78ac00e0bc86
Hoang An <contact@unicode.vn>
2022-12-01 13:01:48 +0700

    Update code

--- react01/src/Components/Props/Posts.js
+++ react01/src/Components/Props/Posts.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Posts({ posts }) {
-  return (
-    <div>
-      <h2>Danh sách bài đăng</h2>
-      {posts.map(({ id, title }) => (
-        <p key={id}>{title}</p>
-      ))}
-    </div>
-  );
+export class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.name = "Hoàng An";
+  }
+  render() {
+    const { posts } = this.props;
+    return (
+      <div>
+        <h2>Danh sách bài đăng: {this.name}</h2>
+        {posts.map(({ id, title }) => (
+          <p key={id}>{title}</p>
+        ))}
+      </div>
+    );
+  }
 }
+
+export default Posts;
