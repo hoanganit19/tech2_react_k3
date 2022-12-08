3dfeed0e4675a8c5b5e228692a25f850fcbe513b
Hoang An <contact@unicode.vn>
2022-12-08 12:11:35 +0700

    Update code

--- react01/src/App.js
+++ react01/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Customers from "./Components/Props/Customers";
 import ParentCom from "./Components/Props/ParentCom";
+import About from "./Components/State/About";
 import ConvertCurrency from "./Components/State/ConvertCurrency/ConvertCurrency";
 import Counter from "./Components/State/Counter";
 import Login from "./Components/State/Login";
@@ -21,7 +22,32 @@ export default function App() {
       {/* <Counter /> */}
       {/* <Login /> */}
       {/* <ConvertCurrency /> */}
+
       <Todos />
     </div>
   );
 }
+
+// export default class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       isPage: "todos",
+//     };
+//   }
+
+//   goAbout = () => {
+//     this.setState({
+//       isPage: "about",
+//     });
+//   };
+//   render() {
+//     const { isPage } = this.state;
+//     return (
+//       <>
+//         <button onClick={this.goAbout}>About</button>
+//         {isPage === "about" ? <About /> : <Todos />}
+//       </>
+//     );
+//   }
+// }
